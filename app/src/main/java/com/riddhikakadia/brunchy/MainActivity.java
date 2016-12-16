54cf0fef424318c1607421ea9c101fb06eb806f0
RKs <riddhik84@gmail.com>
2016-12-16 07:39:43 +0530

    Firebase authentication

--- app/src/main/java/com/riddhikakadia/brunchy/MainActivity.java
+++ app/src/main/java/com/riddhikakadia/brunchy/MainActivity.java
@@ -1,8 +1,11 @@
 package com.riddhikakadia.brunchy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_SIGN_IN = 111;
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    public static String mUsername;
+    public static String mEmail;
+    public static String mPhotoUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +63,57 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mUsername = ANONYMOUS;
+        mEmail = "";
+        mPhotoUrl = "";
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    Log.d(LOG_TAG, "Firebase User: " + mFirebaseAuth.getCurrentUser().toString());
+                    //User logged in
+                    Toast.makeText(getApplicationContext(), getString(R.string.welcome_toast), Toast.LENGTH_SHORT).show();
+                    mUsername = firebaseUser.getDisplayName().toString();
+                    mEmail = firebaseUser.getEmail().toString();
+                    mPhotoUrl = firebaseUser.getPhotoUrl().toString();
+
+                    Log.d(LOG_TAG, "Firebase mUsername " + mUsername + " mEmail " + mEmail);
+                    Log.d(LOG_TAG, "Firebase photo url " + mPhotoUrl);
+                } else {
+                    //User not logged in - got to login screen
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    //TODO change with app logo
+                                    .setLogo(R.mipmap.ic_launcher)
+                                    .setIsSmartLockEnabled(false)
+                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Log.d(LOG_TAG, "RK requestCode RC_SIGN_IN");
+            if (resultCode == RESULT_OK) {
+                Log.d(LOG_TAG, "RK resultCode RESULT_OK sign in");
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(LOG_TAG, "RK resultCode RESULT_CANCELED sign in");
+                Toast.makeText(this, "Sign In Canceled!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
     @Override
@@ -97,5 +171,19 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
     }
 }
